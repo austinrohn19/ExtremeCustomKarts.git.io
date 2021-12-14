efe3cf0e1c62f542af44a6d5c75a282d850243c9
Austin Rohn <Austinrohn19@gmail.com>
2021-12-14 02:16:08 -0500

    update and delete user as an admin

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -221,3 +221,43 @@ exports.getUserDetails = catchAsyncErrors (async (req, res, next) => {
         user
     })
 })
+
+//update user profile Information => /api/v1/admin/user/:id
+exports.updateUser = catchAsyncErrors (async (req, res, next) => {
+
+    //getting the users old data
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    }
+
+    //updating the info of the user
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true
+    })
+})
+
+//delete user profile as Admin => /api/v1/admin/user/:_id
+exports.deleteUser = catchAsyncErrors (async (req, res, next) => {
+
+    const user = await User.findById(req.params.id);
+
+    if(!user){
+        return next(new ErrorHandler(`User not found with id: ${req.params.id}`));
+    }
+
+    //remove avatar from cloudiary - TODO
+
+    await user.remove();
+
+    res.status(200).json({
+        success: true,
+    })
+})
