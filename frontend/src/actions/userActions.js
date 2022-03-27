76ff04ebe67c9fcdc3f8b298e5287165395d359e
Austin Rohn <AustinRohn19@gmail.com>
2022-03-26 21:31:34 -0400

    reducers and actions for registering a user

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
-import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAIL, CLEAR_ERRORS } from '../constants/userConstants'
+import { 
+    LOGIN_REQUEST, 
+    LOGIN_SUCCESS, 
+    LOGIN_FAIL,
+    REGISTER_USER_REQUEST, 
+    REGISTER_USER_SUCCESS, 
+    REGISTER_USER_FAIL, 
+    CLEAR_ERRORS 
+} from '../constants/userConstants'
 
 //Login
 export const login = (email, password) => async (dispatch) => {
@@ -22,7 +30,34 @@ export const login = (email, password) => async (dispatch) => {
 
     } catch (error){
         dispatch({
-            type: 'LOGIN_FAIL',
+            type: LOGIN_FAIL,
+            payload:error.response.data.message
+        })
+    }
+}
+
+// Register User
+export const register = (userData) => async (dispatch) => {
+    try{
+
+        dispatch({type: REGISTER_USER_REQUEST})
+
+        const config = {
+            headers:{
+                'content-type': 'multipart/form-data'
+            }
+        }
+
+        const {data} = await axios.post('/api/v1/register', userData, config)
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+
+    } catch (error){
+        dispatch({
+            type: REGISTER_USER_FAIL,
             payload:error.response.data.message
         })
     }
