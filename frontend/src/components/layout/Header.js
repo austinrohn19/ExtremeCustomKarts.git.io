7b68957bc7c40c292e286ce66112012559cff786
Austin Rohn <AustinRohn19@gmail.com>
2022-03-27 21:46:14 -0400

    displayed the avatar and put a dropdown menu in the header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,11 +1,20 @@
 import React, { Fragment } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { useAlert } from 'react-alert'
 
 import Search from './Search'
 
 import '../../App.css'
 
 const Header = () => {
+
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector(state => state.auth)
+
   return (
     <Fragment>
       <nav class="navbar row">
@@ -20,9 +29,44 @@ const Header = () => {
           <Search />
         </div>
         <div class="col-12 col-md-3 mt-4 mt-md-0 text-center">
-          <Link to="/login" class="btn ml-4" id="login_btn">Login</Link>
-          <span id="cart" class="ml-3">Cart</span>
-          <span class="ml-1" id="cart_count">2</span>
+          <Link to="/cart" style={{ textDecoration: 'none' }}>
+            <span id="cart" class="ml-3">Cart</span>
+            <span class="ml-1" id="cart_count">2</span>
+          </Link>
+
+          {user ? (
+            <div class="ml-4 dropdown d-inline">
+              <Link to="#!" class="btn text-white"
+                type="button" id="dropDownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                <figure class="avatar avatar-nav">
+                  <img
+                    src={user.avatar && user.avatar.url}
+                    alt={user && user.name}
+                    class="rounded-circle"
+                  />
+                </figure>
+                <span>
+                  {user && user.name}
+                </span>
+              </Link>
+            <div class= "dropdown-menu" aria-labelledby="dropDownMenuButton">
+
+              {user && user.role !== 'admin' ? (
+                <Link class="dropdown-item" to="orders/me">Orders</Link>
+              ):(
+                <Link class="dropdown-item" to="/dashboard">Dashboard</Link>
+              )}
+              <Link class="dropdown-item" to="/me">Profile</Link>
+              <Link class="dropdown-item text-danger" to="/">
+                Logout               
+              </Link>
+
+            </div>
+
+            </div>
+          ) : !loading && <Link to="/login" class="btn ml-4" id="login_btn">Login</Link>}
+
         </div>
       </nav>
     </Fragment>
